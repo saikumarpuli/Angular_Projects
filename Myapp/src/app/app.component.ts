568a356d7bf035d1f9feca4c48379dac0cbed297
saikumarpuli <saikumarpuli9@gmail.com>
2019-02-27 19:33:58 +0530

    develop-sample application

--- Myapp/src/app/app.component.ts
+++ Myapp/src/app/app.component.ts
@@ -1,30 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FilterPipe } from 'ngx-filter-pipe';
-
+import { LocalStorage } from '@ngx-pwa/local-storage';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  notes: any;
-  userFilter: any = { name: '' };
-  constructor(private filterPipe: FilterPipe) {
-    console.log(filterPipe.transform(this.records, { name: 'M'}));
+  constructor(private filterPipe: FilterPipe, protected localStorage: LocalStorage) {
+    console.log(filterPipe.transform(this.records, {name: 'M'}));
+    this.localStorage.getItem('user').subscribe((records) => {
+       console.log(this.records);
+    });
   }
-  records = new Array( ' ');
-    date = new Date().toLocaleString();
-   time: any = new Date().toLocaleTimeString();
-   display = 'This is a static text for the notes';
-    delete() {
-    this.records.pop();
-    this.records.pop();
-    this.records.pop();
 
-    }
+  time: any = new Date().toLocaleTimeString();
+  staicnote = this.time + '|' + 'This is a static text for the notes';
+  notes: any;
+  userFilter: any = {name: ''};
+  records = new Array(' ');
+  date = new Date().toLocaleString();
+  i: number;
+
   save() {
-    this.records.push(this.notes);
-    this.records.push(this.time);
-    this.records.push(this.display);
+    this.records[this.records.length] = this.notes;
+    this.records[this.records.length] = this.staicnote;
+    this.notes = '';
+    this.localStorage.setItem('note', JSON.stringify(this.records)).subscribe(() => {});
+
+  }
+
+  touch(note) {
+    this.notes = note;
+  }
+
+  delete() {
+    for (this.i = 0; this.i < this.records.length; this.i++) {
+      if (this.records[this.i] === this.notes) {
+        this.records.splice(this.i, 2);
+        this.localStorage.setItem('note', JSON.stringify(this.records)).subscribe(() => {
+        });
+
+      }
     }
+  }
 }
+
+
