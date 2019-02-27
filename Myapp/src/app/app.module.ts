c2c90d14ecd44897bdb8ad0ddeba576a1679c921
saikumarpuli <saikumarpuli9@gmail.com>
2019-02-27 19:58:57 +0530

    develop-sample application

--- Myapp/src/app/app.module.ts
+++ Myapp/src/app/app.module.ts
@@ -3,12 +3,10 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NotesComponent } from './notes/notes.component';
 import { FilterPipeModule } from 'ngx-filter-pipe'; @NgModule({
   declarations: [
     AppComponent,
-    NotesComponent
-  ],
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
