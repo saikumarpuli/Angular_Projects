d6019f8ba1bcbf083f6158619352be76409a7f29
saikumarpuli <saikumarpuli9@gmail.com>
2019-03-01 19:39:20 +0530

    Movie ticket Booking

--- Myapp/src/app/app-routing.module.ts
+++ Myapp/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 const routes: Routes = [
   {path: 'home', component : HomepageComponent},
   {path: 'login', component : LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '', redirectTo : '/home', pathMatch: 'full'}
 ];
 
