30b124d70a9f7fa5779bf060344aff518d86b3a5
saikumarpuli <saikumarpuli9@gmail.com>
2019-03-05 13:30:05 +0530

    Movie ticket Booking

--- Myapp/src/app/app.module.ts
+++ Myapp/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    MoviesListComponent
   ],
   imports: [
     BrowserModule,
